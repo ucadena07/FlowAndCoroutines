d91686555bb37f419118214edabafcf6d08c68ae
Ulises Cadena <ucadena07@gmail.com>
2024-01-28 09:20:15 -0600

    retries

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -1,14 +1,41 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase6
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class RetryNetworkRequestViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            val numberOfRetries = 2
+            try {
+                repeat(numberOfRetries){
+                    try{
+                        loadRecentVersions()
+                        return@launch
+                    }catch (e: Exception){
+                        Timber.e(e)
+                    }
+                }
+                loadRecentVersions()
+            }catch (e: Exception){
+                Timber.e(e)
+                uiState.value = UiState.Error(e.message!!)
+            }
+
+        }
+    }
+
+    private suspend fun loadRecentVersions() {
+        val versions = api.getRecentAndroidVersions()
+        uiState.value = UiState.Success(versions)
     }
 
 }
