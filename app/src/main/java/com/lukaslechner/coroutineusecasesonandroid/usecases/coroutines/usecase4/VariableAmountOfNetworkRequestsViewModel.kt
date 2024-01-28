5cbd0904395b0367beb702272994488d32d2f080
Ulises Cadena <ucadena07@gmail.com>
2024-01-28 08:56:37 -0600

    case 4 concurrent

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase4/VariableAmountOfNetworkRequestsViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase4/VariableAmountOfNetworkRequestsViewModel.kt
@@ -1,17 +1,49 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase4
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.launch
 
 class VariableAmountOfNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequestsSequentially() {
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            try {
+                val recentVersions = mockApi.getRecentAndroidVersions()
+                val versionFeatures  = recentVersions.map {
+                    mockApi.getAndroidVersionFeatures(it.apiLevel)
+                }
+                uiState.value = UiState.Success(versionFeatures)
+            }catch (e: Exception){
+                uiState.value = UiState.Error(e.localizedMessage!!)
+            }
 
+        }
     }
 
     fun performNetworkRequestsConcurrently() {
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            try {
+                val recentVersions = mockApi.getRecentAndroidVersions()
 
+                val versionFeatures = recentVersions.map {
+                    async {
+                        mockApi.getAndroidVersionFeatures(it.apiLevel)
+                    }
+                }.awaitAll()
+                uiState.value = UiState.Success(versionFeatures)
+            }catch (e: Exception){
+                uiState.value = UiState.Error(e.localizedMessage!!)
+            }
+
+
+        }
     }
 }
