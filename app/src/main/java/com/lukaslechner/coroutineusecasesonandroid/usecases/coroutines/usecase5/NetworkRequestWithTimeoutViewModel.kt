409d0b77d7ef37a586c355bf71847e03b7452ce3
Ulises Cadena <ucadena07@gmail.com>
2024-01-28 09:12:58 -0600

    implementing a timeout

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase5/NetworkRequestWithTimeoutViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase5/NetworkRequestWithTimeoutViewModel.kt
@@ -1,14 +1,62 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase5
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.withTimeoutOrNull
+import timber.log.Timber
 
 class NetworkRequestWithTimeoutViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest(timeout: Long) {
+        uiState.value = UiState.Loading
+        usingWithTimeoutOrNull(timeout)
+    }
 
+    private fun usingWithTimeout(timeout: Long) {
+        viewModelScope.launch {
+            try {
+
+                val versions = withTimeout(timeout) {
+                    api.getRecentAndroidVersions()
+                }
+
+
+                uiState.value = UiState.Success(versions)
+            } catch (timeoutCancellationException: TimeoutCancellationException) {
+                uiState.value = UiState.Error(timeoutCancellationException.message!!)
+            } catch (e: Exception) {
+                Timber.e(e)
+                uiState.value = UiState.Error(e.message!!)
+            }
+
+        }
+    }
+    private fun usingWithTimeoutOrNull(timeout: Long) {
+        viewModelScope.launch {
+            try {
+
+                val versions = withTimeoutOrNull(timeout) {
+                    api.getRecentAndroidVersions()
+                }
+
+                if(versions != null){
+                    uiState.value = UiState.Success(versions)
+                }else{
+                    uiState.value = UiState.Error("timeout error")
+                }
+
+            }  catch (e: Exception) {
+                Timber.e(e)
+                uiState.value = UiState.Error(e.message!!)
+            }
+
+        }
     }
 
 }
