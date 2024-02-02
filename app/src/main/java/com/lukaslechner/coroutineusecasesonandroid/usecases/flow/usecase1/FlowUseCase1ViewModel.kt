17792a4918ea501ac46d5dc3926874000b956176
Ulises Cadena <ucadena07@gmail.com>
2024-02-02 06:04:30 -0600

    displaying stock list on screen

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
@@ -1,12 +1,28 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.flow.usecase1
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import timber.log.Timber
 
 class FlowUseCase1ViewModel(
     stockPriceDataSource: StockPriceDataSource
 ) : BaseViewModel<UiState>() {
 
-    val currentStockPriceAsLiveData: LiveData<UiState> = TODO()
+    val currentStockPriceAsLiveData: MutableLiveData<UiState> = MutableLiveData()
+
+    init {
+        viewModelScope.launch {
+            stockPriceDataSource.latestStockList.collect{
+               currentStockPriceAsLiveData.value = UiState.Success(it)
+            }
+        }
+
+    }
 
 }
