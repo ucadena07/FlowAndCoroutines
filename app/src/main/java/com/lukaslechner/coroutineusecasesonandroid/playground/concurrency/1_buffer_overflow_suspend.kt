746866880e10929937657c845e078865fa86d556
Ulises Cadena <ucadena07@gmail.com>
2024-02-05 06:32:32 -0600

    final commit

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/concurrency/1_buffer_overflow_suspend.kt
@@ -0,0 +1,27 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.concurrency
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.coroutineScope
+import  kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+
+suspend fun main() = coroutineScope {
+    val flow = flow{
+        kotlin.repeat(5){
+            val pancake = it + 1
+            kotlin.io.println("Emitter: start cooking pancake $pancake")
+            kotlinx.coroutines.delay(100)
+            kotlin.io.println("Emitter: pancake $pancake ready")
+            emit(pancake)
+        }
+    }.buffer(capacity = 1, onBufferOverflow = BufferOverflow.SUSPEND)
+
+    flow.collect{
+        println("collector: start eating pancake $it")
+        delay(300)
+        println("collector: finished eating pancake $it")
+    }
+
+}
+
